a883cc22fa3f299e363cd04e1893843e0249c3fc
laila1996 <123996735+laila1996@users.noreply.github.com>
2023-05-02 20:28:23 -0400

    notes

---
+++ src/webapp/notes.java
@@ -0,0 +1,10 @@
+package webapp;
+
+public class notes {
+    /*add = I think its commit same job because didn't see that add
+checkout = when u are working on a specific branch you must check-out it (it works the opposite of the English meaning of that word )
+branch = is when basically your own version of the local repository that you will be working at and once its done you can merge it to the master repository (to avoid any mess )
+push = its the share of the local repository work to the remote repository of the origin master (after asking the permeation to merge )
+
+     */
+}
